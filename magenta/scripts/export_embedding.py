3d7a42a1b32d8d8ebfffdcc5ce97ac00836f9a1f
Eric Wolf <eric.wolf123@gmail.com>
2017-01-05 04:57:27 +0800

    Added export embedding script

---
+++ magenta/scripts/export_embedding.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import os
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('run_dir', '/tmp/melody_rnn/logdir/run1',
+                           'Path to the directory where checkpoints and '
+                           'summary events will be saved during training and '
+                           'evaluation. Separate subdirectories for training '
+                           'events and eval events will be created within '
+                           '`run_dir`. Multiple runs can be stored within the '
+                           'parent directory of `run_dir`. Point TensorBoard '
+                           'to the parent directory of `run_dir` to see all '
+                           'your runs.')
+
+if not FLAGS.run_dir:
+	tf.logging.fatal('--run_dir required')
+	return
+
+run_dir = os.path.expanduser(FLAGS.run_dir)
+train_dir = os.path.join(run_dir, 'train')
+
+with tf.Graph().as_default() as g:
+  with tf.Session() as sess:
+  	# Trick to load variable without loading full graph: use validate_shape=False
+  	# However, this also has to be set in the graph
+    embedding = tf.Variable(0, name='embedding', validate_shape=False)
+
+    # Load data
+    saver = tf.train.Saver()
+    checkpoint_path = tf.train.latest_checkpoint(train_dir)
+    saver.restore(sess, checkpoint_path)
+
+    # Write numpy file
+    embedding_ = sess.run(embedding)
+    np.save('...', embedding)
