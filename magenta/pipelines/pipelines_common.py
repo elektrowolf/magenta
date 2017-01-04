38ebf45ab4137e694a3546e8649bcdc19e0afb4b
Eric Wolf <eric.wolf123@gmail.com>
2017-01-04 23:52:13 +0800

    Added import

--- magenta/pipelines/pipelines_common.py
+++ magenta/pipelines/pipelines_common.py
@@ -20,6 +20,7 @@ import numpy as np
 import tensorflow as tf
 
 from magenta.music import sequences_lib
+from magenta.music import events_lib
 from magenta.pipelines import pipeline
 from magenta.pipelines import statistics
 from magenta.protobuf import music_pb2
