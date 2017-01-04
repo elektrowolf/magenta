319eda15d4b4465fa9c88dac59277b94db0b5aa6
Eric Wolf <eric.wolf123@gmail.com>
2017-01-04 23:33:38 +0800

    Moved ID pipeline to pipelines_common

--- magenta/pipelines/pipelines_common.py
+++ magenta/pipelines/pipelines_common.py
@@ -103,3 +103,24 @@ class RandomPartition(pipeline.Pipeline):
 
   def _make_stats(self, increment_partition=None):
     return [statistics.Counter(increment_partition + '_count', 1)]
+
+class IDPipeline(pipeline.Pipeline):
+  """A Pipeline that assigns unique IDs to EventSequences and logs the ID mappings in the pipeline stats."""
+
+  def __init__(self, name=None):
+    super(IDPipeline, self).__init__(
+        input_type=events_lib.EventSequence,
+        output_type=events_lib.EventSequence,
+        name=name)
+    self.counter = 0
+    self._set_stats({})
+
+  def transform(self, event_sequence):
+    mappings = self.get_stats()
+    mappings[self.counter] = event_sequence.filename
+    self._set_stats(mappings)
+
+    event_sequence.id = event_sequence
+  self.counter++
+
+  return [event_sequence]
