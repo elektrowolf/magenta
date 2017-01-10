f1041a7046c34d0f64c1c929ae6084016d9dfb8f
Eric Wolf <eric.wolf123@gmail.com>
2017-01-10 15:56:58 +0800

    Multi file export

--- magenta/models/melody_rnn/melody_rnn_export_training_melody.py
+++ magenta/models/melody_rnn/melody_rnn_export_training_melody.py
@@ -63,6 +63,8 @@ def find_records(needed_ids, config, sequence_example_file_paths):
           tf.logging.warn('Hit end, could not locate records %s' % needed_ids.join(', '))
           return result
 
+        print id_[0], needed_ids
+
         if int(id_[0]) in needed_ids:
           tf.logging.info('Found %d' % id_[0])
           result[id_[0]] = labels_[0]
